84616932a1b02e721dc6b96e37dade8264403518
Bernice Lam <bernicelam912@gmail.com>
2026-03-12 20:45:40 -0400

    dynamic node allocation initial commit

--- targets/single_servo_driver/rev1/Core/Inc/pinecan_handlers.h
+++ targets/single_servo_driver/rev1/Core/Inc/pinecan_handlers.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "canard.h"
+#include "dynamic_node_allocatee.h"
 
 // REGISTER_RX_HANDLER(TYPE, HANDLER, TRANSFER_KIND)
 // TYPE is the base name of the DSDL type:
@@ -8,6 +9,7 @@
 // HANDLER is the function that handles the received transfer
 // TRANSFER_KIND is one of: REQUEST, RESPONSE, BROADCAST
 #define RX_HANDLER_LIST \
-    REGISTER_RX_HANDLER(UAVCAN_EQUIPMENT_ACTUATOR_ARRAYCOMMAND, handleArrayCommand, BROADCAST)
+    REGISTER_RX_HANDLER(UAVCAN_EQUIPMENT_ACTUATOR_ARRAYCOMMAND, handleArrayCommand, BROADCAST) \
+    REGISTER_RX_HANDLER(UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION, handleDnIDAllocationRes, BROADCAST)
 
 void handleArrayCommand(CanardInstance* ins, CanardRxTransfer *transfer);
