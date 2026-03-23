942f687a720f97c046774ed5f23fbda8d25d5834
f24shen <f24shen@uwaterloo.ca>
2026-03-22 20:07:54 -0400

    updated comments

--- pinecan/boards/stm32l4/pinecanBoard.c
+++ pinecan/boards/stm32l4/pinecanBoard.c
@@ -83,14 +83,7 @@ bool processTxMessage(const CanardCANFrame *txFrame) {
 
 /* ============ EXTERNAL PUBLIC FUNCTION DEFINITIONS ============ */
 
-// TODO: compare to const int16_t rx_res = canardSTM32Recieve(hcan, CAN_RX_FIFO0, &rx_frame); again
-// Can think abt support for multiple rx fifos (this would come with some smart stuff with setting up the filters correctly for that as well)
-// Also even though we can use our own rx fifo queue here, we can also just use the fact that stm32 already has an rx fifo of length 3 (and its in hardware hooray)
-
-//create another queue (holds more than 3 frames) which takes frames from RXFIFO0, holds frames until they can be processed
-//figure out how to make queue global
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
-//end goal: create frame, put in buffer and return
 {
     CAN_RxHeaderTypeDef rxHeader = {0};
     uint8_t rxData[8] = {0};
